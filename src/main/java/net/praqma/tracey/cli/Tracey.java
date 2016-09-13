c438ad11459398589dc3f5e1cb878c47bef4255f
Andrey Devyatkin <ady@praqma.net>
2016-09-13 14:22:29 +0200

    Fix #5: Add support for additional routing metadata
    Pull request  #8

--- src/main/java/net/praqma/tracey/cli/Tracey.java
+++ src/main/java/net/praqma/tracey/cli/Tracey.java
@@ -1,26 +1,21 @@
 package net.praqma.tracey.cli;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import net.praqma.tracey.broker.rabbitmq.TraceyRabbitMQBrokerImpl;
-import net.praqma.tracey.broker.rabbitmq.TraceyRabbitMQBrokerImpl.ExchangeType;
+import net.praqma.tracey.broker.impl.rabbitmq.RabbitMQDefaults;
+import net.praqma.tracey.broker.impl.rabbitmq.RabbitMQRoutingInfo;
+import net.praqma.tracey.broker.impl.rabbitmq.TraceyRabbitMQBrokerImpl;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Tracey {
 
-    //Sensible defaults:
-    static String host = "localhost";
-    static String exchange = "tracey";
-    static String user = "guest";
-    static String pw = "guest";
-    static int port = 5672;
-    static ExchangeType type = TraceyRabbitMQBrokerImpl.ExchangeType.TOPIC;
-
-    static TraceyRabbitMQBrokerImpl broker;
+    private static TraceyRabbitMQBrokerImpl broker;
+    private static RabbitMQRoutingInfo routingInfo;
 
     public static String readFileToString(String path) throws IOException {
         String content = new String(Files.readAllBytes(Paths.get(path)));
@@ -29,14 +24,27 @@ public class Tracey {
 
     public static void main(String[] args) throws Exception {
         ArgumentParser parser = ArgumentParsers.newArgumentParser("tracey")
-        .description("Send message with Tracey. Using RabbitMQ\n\nRun with arguments"
-                + " 'say -h' for help in sending messages\nRun with arguments 'listen -h' for help receiving messages");
+        .description("Send message with Tracey. Using RabbitMQ\n\nRun with arguments" +
+                "'say -h' for help in sending messages\nRun with arguments 'listen -h' for help receiving messages.\n" +
+                "You may use environment variable names for user name and password to avoid storing\n " +
+                "them in plain text. Use the following format - %SOMETEXT%, ${SOMETEXT}, $SOMETEXT, $SOMETEXT$.\n" +
+                "Please notice command line options will override values provided through the configuration file\n" +
+                "If no arguments will be provided, a connection will be configured with default values:\n" +
+                String.format("host: %s", RabbitMQDefaults.HOST) + "\n" +
+                String.format("port: %s", RabbitMQDefaults.PORT) + "\n" +
+                String.format("user: %s", RabbitMQDefaults.USERNAME) + "\n" +
+                String.format("password: %s", RabbitMQDefaults.PASSWORD) + "\n" +
+                String.format("exchange type: %s", RabbitMQDefaults.EXCHANGE_TYPE) + "\n" +
+                String.format("exchange name: %s", RabbitMQDefaults.EXCHANGE_NAME) + "\n" +
+                String.format("delivery mode: %s", RabbitMQDefaults.DELEIVERY_MODE) + "\n" +
+                String.format("headers: %s", RabbitMQDefaults.HEADERS) + "\n"
 
+        );
         Subparsers subparsers = parser.addSubparsers();
-        Subparser sayParser = subparsers.addParser("say").defaultHelp(true);
+        Subparser sayParser = subparsers.addParser("say");
         sayParser.addArgument("message");
         sayParser.addArgument("-n", "--node").dest("node").help("The URL of the RabbitMQ server");
-        sayParser.addArgument("-e", "--exchange").setDefault("tracey").help("Exhange name");
+        sayParser.addArgument("-e", "--exchange").help("Exhange name");
         sayParser.addArgument("-u", "--user").dest("user").help("Username");
         sayParser.addArgument("-s", "--secret").dest("secret").help("Password");
         sayParser.addArgument("-p", "--port").dest("port").type(Integer.class).help("Port");
@@ -59,51 +67,48 @@ public class Tracey {
             System.exit(1);
         }
 
-        broker = new TraceyRabbitMQBrokerImpl(host, user, user, type, exchange);
+        broker = new TraceyRabbitMQBrokerImpl();
+        routingInfo = new RabbitMQRoutingInfo();
 
         if(ns.getString("config") != null) {
-            broker = new TraceyRabbitMQBrokerImpl(new File(ns.getString("config")));
+            File config = new File(ns.getString("config"));
+            broker = new TraceyRabbitMQBrokerImpl(config);
+            routingInfo = RabbitMQRoutingInfo.buildFromConfigFile(config);
         }
 
         if(ns.getString("node") != null) {
-            broker.getReceiver().setHost(ns.getString("node"));
-            broker.getSender().setHost(ns.getString("node"));
+            broker.getReceiver().getConnection().setHost(ns.getString("node"));
+            broker.getSender().getConnection().setHost(ns.getString("node"));
         }
 
         if(ns.getString("secret") != null) {
-            broker.getReceiver().setPassword(ns.getString("secret"));
-            broker.getSender().setPw(ns.getString("secret"));
+            broker.getReceiver().getConnection().setPassword(ns.getString("secret"));
+            broker.getSender().getConnection().setPassword(ns.getString("secret"));
         }
 
         if(ns.getString("user") != null) {
-            broker.getReceiver().setUsername(ns.getString("user"));
-            broker.getSender().setUsername(ns.getString("user"));
+            broker.getReceiver().getConnection().setUserName(ns.getString("user"));
+            broker.getSender().getConnection().setUserName(ns.getString("user"));
         }
 
         if(ns.getInt("port") != null) {
-            broker.getReceiver().setPort(ns.getInt("port"));
-            broker.getSender().setPort(ns.getInt("port"));
+            broker.getReceiver().getConnection().setPort(ns.getInt("port"));
+            broker.getSender().getConnection().setPort(ns.getInt("port"));
         }
 
         if(ns.getString("type") != null) {
-            broker.getReceiver().setType(ExchangeType.valueOf(ns.getString("type")));
-            broker.getSender().setType(ExchangeType.valueOf(ns.getString("type")));
+            routingInfo.setExchangeType(ns.getString("type"));
         }
 
         if(ns.getString("exchange") != null) {
-            broker.getReceiver().setExchange(ns.getString("exchange"));
+            routingInfo.setExchangeName(ns.getString("exchange"));
         }
-
-        broker.configure();
 
         String message = ns.getString("message");
 
         if(message == null) {
-            if(ns.getString("exchange") != null) {
-                broker.receive(ns.getString("exchange"));
-            } else {
-                broker.receive(exchange);
-            }
+            broker.receive(routingInfo);
+
         } else {
 
             String actualMessage = message;
@@ -111,15 +116,7 @@ public class Tracey {
             if(ns.getBoolean("payload")) {
                 actualMessage = readFileToString(message);
             }
-
-            if(ns.getString("exchange") != null) {
-                broker.send(actualMessage, ns.getString("exchange"));
-            } else {
-                //If no exchange is specifed. Use the value from the receiver. We assume
-                //that we listen and send to the same exchange.
-                broker.send(actualMessage, broker.getReceiver().getExchange());
-
-            }
+            broker.send(actualMessage, routingInfo);
         }
     }
 }
